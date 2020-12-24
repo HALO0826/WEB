e358dad164485d460ae85a7e70c62e484fbbfa40
Zyq <1343325715@qq.com>
2020-12-24 10:20:01 +0800

    前端服务器

---
+++ client/src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App'
+import store from './store'
+
+// 引入路由器
+import router from './router/index'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 配置图片懒加载
+import VueLazyload from 'vue-lazyload'
+import loading from './common/img/loading.gif'
+Vue.use(VueLazyload, {
+  loading
+});
+
+import './config/filters'
+// 引入bootstrap
+import 'jquery'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+new Vue({
+  el:'#app',
+  router,
+  store,
+  render: h=>h(App)
+});
